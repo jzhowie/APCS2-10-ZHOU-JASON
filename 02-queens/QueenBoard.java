1034ed75958eadfe6315ddde7d516f14a4383614
jzhowie <jzhou20@stuy.edu>
2021-02-11 14:41:58 -0500

    Initialized addQueen + adds spots that are in danger horizontally

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -1,7 +1,18 @@
+// TODO: toString(), solve(), countSolutions(), add & remove Queen()
+// https://konstantinnovation.github.io/apcs.html#20210210
+// ISSUES: TBD
 public class QueenBoard {
 
 private int[][] board;
 private int QUEEN = -1;
+
+public boolean addQueen(int r, int c) { //remember to switch back
+	board[r-1][c-1] = QUEEN;
+	for (int i = c; i < board[r-1].length; i++) {
+		board[r-1][i]++;
+	}
+	return true;
+}
 
 public QueenBoard(int size) {
 	board = new int[size][size];
