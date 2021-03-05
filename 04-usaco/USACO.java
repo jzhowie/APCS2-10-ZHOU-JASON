791dd996be39e07b9e755c4d8128141787d8ae53
jzhowie <jzhou20@stuy.edu>
2021-03-05 14:50:47 -0500

    Initialized helper method to help with "flattening" array and tester funct

--- 04-usaco/USACO.java
+++ 04-usaco/USACO.java
@@ -4,19 +4,66 @@ import java.io.*;
 public class USACO {
 public static int bronze(String filename) throws FileNotFoundException {
 	Scanner scan = new Scanner(new File(filename));
-	Scanner line = new Scanner(scan.next());
+	Scanner line = new Scanner(scan.nextLine());
 
 	int rows = line.nextInt();
 	int cols = line.nextInt();
 	int elevation = line.nextInt();
 	int instructions = line.nextInt();
 
-	System.out.println(rows);
-	System.out.println(cols);
-	System.out.println(elevation);
-	System.out.println(instructions);
+	int[][] field = new int[rows][cols];
+
+	for (int i = 0; i < rows; i++) {
+		line = new Scanner(scan.nextLine());
+		for (int j = 0; j < cols; j++) {
+			field[i][j] = line.nextInt();
+		}
+	}
+
+	for (int i = 0; i < instructions; i++) {
+		line = new Scanner(scan.nextLine());
+		int row_s = line.nextInt();
+		int col_s = line.nextInt();
+		int smash = line.nextInt();
+		field = bronzeHelper (row_s, col_s, smash, field);
+
+	}
+
 	return -1;
 }
+
+private static int[][] bronzeHelper (int row, int col, int depth, int[][] board) {
+	int[] coords = {row - 1, col - 1};
+	for (int i = row - 1; i < row + 2; i++) {
+		for (int j = col - 1; j < col + 2; j++) {
+			if (board[i][j] > board[coords[0]][coords[1]]) {
+				coords = new int[] {i, j};
+			}
+		}
+	}
+
+	board[coords[0]][coords[1]] -= depth;
+	printArr(board);
+
+	for (int i = row - 1; i < row + 2; i++) {
+		for (int j = col - 1; j < col + 2; j++) {
+			if (board[i][j] > board[coords[0]][coords[1]]) {
+				board[i][j] = board[coords[0]][coords[1]];
+			}
+		}
+	}
+
+	printArr(board);
+	return board;
+}
+
+public static void printArr(int[][] arr) {
+	for (int i = 0; i < arr.length; i++) {
+		System.out.println(Arrays.toString(arr[i]));
+	}
+	System.out.println();
+}
+
 public static long silver(String filename) throws FileNotFoundException {
 	return -1;
 }
