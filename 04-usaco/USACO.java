1dcf1f7d65e3affbddaece6309f875d18220f2d2
jzhowie <jzhou20@stuy.edu>
2021-03-06 13:17:04 -0500

    Prettied up code + privated helper methods

--- 04-usaco/USACO.java
+++ 04-usaco/USACO.java
@@ -29,11 +29,9 @@ public static int bronze(String filename) throws FileNotFoundException {
 	}
 
 	int totalDepth = 0;
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < cols; j++) {
-			if (elevation - field[i][j] > 0) {
-				totalDepth = totalDepth + (elevation - field[i][j]);
-			}
+	for (int[] row : field) {
+		for (int col : row) {
+			if (elevation - col > 0) totalDepth = totalDepth + (elevation - col);
 		}
 	}
 	return totalDepth * 72 * 72;
@@ -43,18 +41,14 @@ private static int[][] bronzeHelper(int row, int col, int depth, int[][] board) 
 	int[] coords = {row - 1, col - 1};
 	for (int i = row - 1; i < row + 2; i++) {
 		for (int j = col - 1; j < col + 2; j++) {
-			if (board[i][j] > board[coords[0]][coords[1]]) {
-				coords = new int[] {i, j};
-			}
+			if (board[i][j] > board[coords[0]][coords[1]]) coords = new int[] {i, j};
 		}
 	}
 
 	board[coords[0]][coords[1]] -= depth;
 	for (int i = row - 1; i < row + 2; i++) {
 		for (int j = col - 1; j < col + 2; j++) {
-			if (board[i][j] > board[coords[0]][coords[1]]) {
-				board[i][j] = board[coords[0]][coords[1]];
-			}
+			if (board[i][j] > board[coords[0]][coords[1]]) board[i][j] = board[coords[0]][coords[1]];
 		}
 	}
 	return board;
@@ -109,35 +103,24 @@ public static long silver(String filename) throws FileNotFoundException {
 		for (int row_t = 0; row_t < rows; row_t++) {
 			for (int col_t = 0; col_t < cols; col_t++) {
 				long temp = silverHelper(row_t, col_t, field[sec]);
-				if (temp > 0) {
-					field[sec+1][row_t][col_t] = temp;
-				}
+				if (temp > 0) field[sec+1][row_t][col_t] = temp;
 			}
 		}
 	}
+
 	return field[time][endR-1][endC-1];
 }
 
-public static long silverHelper(int row, int col, long[][] board) {
+private static long silverHelper(int row, int col, long[][] board) {
 	long counter = 0;
-	if (board[row][col] == -1) {
-		return -1;
-	}
-	if (board[row][col] != 0) {
-		return 0;
-	}
-	if (col+1 < board[0].length && board[row][col+1] != -1) {
-		counter += board[row][col+1];
-	}
-	if (col-1 >= 0 && board[row][col-1] != -1) {
-		counter += board[row][col-1];
-	}
-	if (row-1 >= 0 && board[row-1][col] != -1) {
-		counter += board[row-1][col];
-	}
-	if (row+1 < board.length && board[row+1][col] != -1) {
-		counter += board[row+1][col];
-	}
+	if (board[row][col] == -1) return -1;
+	if (board[row][col] != 0) return 0;
+
+	if (col+1 < board[0].length && board[row][col+1] != -1) counter += board[row][col+1];
+	if (col-1 >= 0 && board[row][col-1] != -1) counter += board[row][col-1];
+	if (row-1 >= 0 && board[row-1][col] != -1) counter += board[row-1][col];
+	if (row+1 < board.length && board[row+1][col] != -1) counter += board[row+1][col];
+
 	return counter;
 }
 }
