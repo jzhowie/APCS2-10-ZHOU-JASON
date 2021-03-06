6eb29ee9dd5d06c0cf06dfc4591b3e2840af74dd
jzhowie <jzhou20@stuy.edu>
2021-03-06 13:22:48 -0500

    Commented out tester methods

--- 04-usaco/USACO.java
+++ 04-usaco/USACO.java
@@ -54,22 +54,22 @@ private static int[][] bronzeHelper(int row, int col, int depth, int[][] board) 
 	return board;
 }
 
-public static void printArr(int[][] arr) {
-	for (int i = 0; i < arr.length; i++) {
-		System.out.println(Arrays.toString(arr[i]));
-	}
-	System.out.println();
-}
-
-public static void printArr(long[][][] arr) {
-	for (int i = 0; i < arr.length; i++) {
-		for (int j = 0; j < arr[i].length; j++) {
-			System.out.println(Arrays.toString(arr[i][j]));
-		}
-		System.out.println();
-	}
-	System.out.println();
-}
+// public static void printArr(int[][] arr) {
+// 	for (int i = 0; i < arr.length; i++) {
+// 		System.out.println(Arrays.toString(arr[i]));
+// 	}
+// 	System.out.println();
+// }
+//
+// public static void printArr(long[][][] arr) {
+// 	for (int i = 0; i < arr.length; i++) {
+// 		for (int j = 0; j < arr[i].length; j++) {
+// 			System.out.println(Arrays.toString(arr[i][j]));
+// 		}
+// 		System.out.println();
+// 	}
+// 	System.out.println();
+// }
 
 public static long silver(String filename) throws FileNotFoundException {
 	Scanner scan = new Scanner(new File(filename));
