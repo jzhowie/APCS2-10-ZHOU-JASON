215361c013d37e7ca95a189340733b77535bb477
jzhowie <jzhou20@stuy.edu>
2021-03-09 15:55:45 -0500

    Partition picks a random index

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -3,7 +3,8 @@ import java.util.*;
 
 public class Preliminary {
 public static int partition (int[] data, int start, int end) {
-	int pivot = 2;
+	int pivot = (int) (Math.random() * 5);
+	// System.out.println(data[pivot]);
 	int first = start + 1;
 	int last = end;
 
