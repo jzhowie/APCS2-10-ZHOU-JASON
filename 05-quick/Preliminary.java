24b389c892976812973906c26ac1b6bcd51b0b5d
jzhowie <jzhou20@stuy.edu>
2021-03-10 12:55:57 -0500

    Added random for all situations of array, returns correct index of pivot

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -3,8 +3,7 @@ import java.util.*;
 
 public class Preliminary {
 public static int partition (int[] data, int start, int end) {
-	int pivot = (int) (Math.random() * 5);
-	// System.out.println(data[pivot]);
+	int pivot = (int) (Math.random() * (end + 1 - start)) + start;
 	int first = start + 1;
 	int last = end;
 
@@ -29,6 +28,7 @@ public static int partition (int[] data, int start, int end) {
 		temp = data[first-1];
 		data[first-1] = data[start];
 		data[start] = temp;
+		return first-1;
 	}
 	else {
 		temp = data[first];
