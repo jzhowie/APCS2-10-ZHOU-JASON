94f389e1b0d1b8e50a7135c764bb814cec4dec5e
jzhowie <jzhou20@stuy.edu>
2021-03-17 14:17:54 -0400

    Implemented recursive parititioning for sort

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -1,10 +1,16 @@
 public class Merge {
 public static void mergesort(int[] data) {
 	int[] temp = new int[data.length];
-	mergesort(data, temp, 0, data.length);
+	mergesort(data, temp, 0, data.length - 1);
 }
 
 public static void mergesort(int[] data, int[] temp, int lo, int hi) {
-
+	System.out.println("(low " + lo + " high " + hi + ")");
+	if (lo == hi) {
+		return;
+	}
+	mergesort(temp, data, lo, lo + (hi - lo) / 2);
+	mergesort(temp, data, lo + ((hi - lo) / 2) + 1, hi);
+	if (lo == 0 && hi == data.length) return;
 }
 }
