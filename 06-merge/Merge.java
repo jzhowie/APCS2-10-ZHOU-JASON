970819813a7c48dac66f22deaea97e0c00b9ceec
jzhowie <jzhou20@stuy.edu>
2021-03-17 13:59:10 -0400

    Initialized mergesort

---
+++ 06-merge/Merge.java
@@ -0,0 +1,10 @@
+public class Merge {
+public static void mergesort(int[] data) {
+	int[] temp = new int[data.length];
+	mergesort(data, temp, 0, data.length);
+}
+
+public static void mergesort(int[] data, int[] temp, int lo, int hi) {
+
+}
+}
