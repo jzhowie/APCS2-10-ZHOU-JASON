b1c7c8098f65591e05584c6a64684f35e499459a
jzhowie <jzhou20@stuy.edu>
2021-03-20 10:37:37 -0400

    Added add (to front or back) functions

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -1,6 +1,6 @@
 //TODO: Everyting + exceptions
 //ISSUES: TBD
-//NOTES: For testing; ensure start and end can wrap around array
+//NOTES: For testing; ensure start and end can wrap around array, check for constant time
 
 public class MyDeque<E> {
 private E[] data;
@@ -8,8 +8,8 @@ private int size, start, end;
 
 public MyDeque() {
 	size = 10;
-	start = size / 2;
-	end = size / 2;
+	start = 5;
+	end = 6;
 
 	@SuppressWarnings("unchecked")
 	E[] d = (E[]) new Object[size];
@@ -19,7 +19,7 @@ public MyDeque() {
 public MyDeque(int initialCapacity) {
 	size = initialCapacity;
 	start = size / 2;
-	end = size / 2;
+	end = (size / 2) + 1;
 
 	@SuppressWarnings("unchecked")
 	E[] d = (E[]) new Object[size];
@@ -31,18 +31,44 @@ public int size() {
 }
 
 public String toString() {
-	return ""; //ph
+	String value = "{";
+	int index = start + 1;
+	while (index != end) {
+		if (index == size()) {
+			index = 0;
+		}
+		value += data[index];
+		if (index != end - 1) {
+			value += ", ";
+		}
+		index++;
+	}
+	value += "}";
+	return value;
 }
 
-public void addFirst(E element) {
-
+public void addFirst(E element) { // add resizing
+	if (start == -1) {
+		start = size() - 1;
+	}
+	data[start] = element;
+	start--;
 }
 
-public void addLast(E element) {
-
+public void addLast(E element) { //add resizing
+	if (end == size()) {
+		end = 0;
+	}
+	data[end] = element;
+	end++;
 }
 
 public E removeFirst() {
+	if (start == -1) {
+		return data[size() - 1];
+
+
+	}
 	return data[0]; //ph
 }
 
@@ -51,10 +77,22 @@ public E removeLast() {
 }
 
 public E getFirst() {
-	return data[0]; //ph
+	if (start == -1) {
+		return data[size() - 1];
+	}
+	if (start == size() - 1) {
+		return data[0];
+	}
+	return data[start+1]; //ph
 }
 
 public E getLast() {
-	return data[0]; //ph
+	if (end == size()) {
+		return data[size() - 1];
+	}
+	if (end == 0) {
+		return data[size() - 1];
+	}
+	return data[end-1]; //ph
 }
 }
