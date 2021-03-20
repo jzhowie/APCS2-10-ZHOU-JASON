d2b04eab847d299a68444eaf8ff47e3734d3c363
jzhowie <jzhou20@stuy.edu>
2021-03-20 09:50:14 -0400

    Initialized constructors + size method in MyDeque

---
+++ 07-mydeque/MyDeque.java
@@ -0,0 +1,54 @@
+//TODO: Everyting + exceptions
+//ISSUES: TBD
+//NOTES: For testing; ensure start and end can wrap around array
+
+public class MyDeque<E> {
+private E[] data;
+private int size, start, end;
+
+public MyDeque() {
+	size = 10;
+	@SuppressWarnings("unchecked")
+	E[] d = (E[]) new Object[size];
+	data = d;
+}
+
+public MyDeque(int initialCapacity) {
+	size = initialCapacity;
+	@SuppressWarnings("unchecked")
+	E[] d = (E[]) new Object[size];
+	data = d;
+}
+
+public int size() {
+	return size;
+}
+
+public String toString() {
+	return ""; //ph
+}
+
+public void addFirst(E element) {
+
+}
+
+public void addLast(E element) {
+
+}
+
+public E removeFirst() {
+	return data[0]; //ph
+}
+
+public E removeLast() {
+	return data[0]; //ph
+}
+
+public E getFirst() {
+	return data[0]; //ph
+}
+
+public E getLast() {
+	return data[0]; //ph
+}
+}
