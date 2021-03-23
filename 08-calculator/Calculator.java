0e6ffe69d8d13dccff3a4caffa29a0a11fcfc977
jzhowie <jzhou20@stuy.edu>
2021-03-23 14:30:06 -0400

    Added calculation portion of eval

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -4,18 +4,20 @@ public static double eval(String s) {
 	String[] splited = s.split("\\s+");
 	ArrayDeque<Double> calc = new ArrayDeque<Double>();
 	for (String g : splited) {
-		// System.out.println(g);
+		System.out.println(g);
 		try {
 			Double temp = Double.parseDouble(g);
+			calc.addLast(temp);
 		}
 		catch (Exception NumberFormatException) {
-			if (g.equals("+")) return 1;
-			else if (g.equals("-")) return 1;
-			else if (g.equals("*")) return 1;
-			else if (g.equals("/")) return 1;
-			else if (g.equals("%")) return 1;
+			System.out.println(calc.toString());
+			if (g.equals("+")) calc.addLast(calc.removeLast() + calc.removeLast());
+			else if (g.equals("-")) calc.addLast(calc.removeLast() - calc.removeLast());
+			else if (g.equals("*")) calc.addLast(calc.removeLast() * calc.removeLast());
+			else if (g.equals("/")) calc.addLast(calc.removeLast() / calc.removeLast());
+			else if (g.equals("%")) calc.addLast(calc.removeLast() % calc.removeLast());
 		}
 	}
-	return -1;
+	return calc.getLast();
 }
 }
