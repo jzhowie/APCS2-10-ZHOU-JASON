441de4ab404b170a4454104422d65b2a2133db5c
jzhowie <jzhou20@stuy.edu>
2021-03-23 14:41:53 -0400

    Added exceptions for too few, too many operands and empty strings

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -2,23 +2,25 @@ import java.util.*;
 public class Calculator {
 public static double eval(String s) {
 	String[] splited = s.split("\\s+");
+	if (splited.length < 1) throw new IllegalArgumentException("Empty string");
 	ArrayDeque<Double> calc = new ArrayDeque<Double>();
 	for (String g : splited) {
-		System.out.println(g);
 		try {
 			Double temp = Double.parseDouble(g);
 			calc.addLast(temp);
 		}
 		catch (Exception NumberFormatException) {
 			Double temp = calc.removeLast();
+			if (calc.size() < 2) throw new IllegalArgumentException("Too few operands");
+
 			if (g.equals("+")) calc.addLast(calc.removeLast() + temp);
 			else if (g.equals("-")) calc.addLast(calc.removeLast() - temp);
 			else if (g.equals("*")) calc.addLast(calc.removeLast() * temp);
 			else if (g.equals("/")) calc.addLast(calc.removeLast() / temp);
 			else if (g.equals("%")) calc.addLast(calc.removeLast() % temp);
-
 		}
 	}
+	if (calc.size() != 1) throw new IllegalArgumentException("Too many operands");
 	return calc.getLast();
 }
 }
