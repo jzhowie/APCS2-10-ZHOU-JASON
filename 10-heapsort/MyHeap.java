e9471456972c93119d36268bd843f24332d3919a
jzhowie <jzhou20@stuy.edu>
2021-04-22 15:09:27 -0400

    Fixed infinite loop when you attempt to pushDown element of a valid heap, initialized buildHeap

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -1,9 +1,12 @@
-//NOTES: Scenarios, two elements out of size, one element outside of size (we need to get function to continue as this), none
+//TODO:
+//ISSUES:
+//NOTES:
+
 import java.util.*;
 public class MyHeap {
   public static void main (String[] args) {
     int[] test = {-19, 1, 1, -5, -3};
-    pushDown(test, 4, 0);
+    buildHeap(test);
     System.out.println(Arrays.toString(test));
   }
 
@@ -16,7 +19,7 @@ public class MyHeap {
           data[index] = temp;
           index = index*2+2;
         }
-        
+
         else {
           int temp = data[index*2+1];
           data[index*2+1] = data[index];
@@ -24,6 +27,16 @@ public class MyHeap {
           index = index*2+1;
         }
       }
+
+      else {
+        return;
+      }
+    }
+  }
+
+  public static void buildHeap(int[] data) {
+    for (int i = data.length - 1; i >= 0; i--) {
+      pushDown(data, data.length, i);
     }
   }
 }
