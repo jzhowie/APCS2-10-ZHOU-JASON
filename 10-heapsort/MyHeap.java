9c13818b9fbbe5f7bbb43018ac067de094619ba4
jzhowie <jzhou20@stuy.edu>
2021-04-27 13:03:59 -0400

    Privated helper methods for heapsort

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -1,15 +1,4 @@
-//TODO:
-//ISSUES:
-//NOTES:
-
-import java.util.*;
 public class MyHeap {
-  public static void main (String[] args) {
-    int[] test = {23, 1, 6, 19, 14, 18, 8, 24, 15};
-    heapsort(test);
-    System.out.println(Arrays.toString(test));
-  }
-
   private static void pushDown(int[] data, int size, int index) {
     while (index * 2 + 1 < size) {
       if (data[index] < data[index*2+1] || (index*2+2 < size && data[index] < data[index*2+2])) {
@@ -32,7 +21,7 @@ public class MyHeap {
     }
   }
 
-  public static void buildHeap(int[] data) {
+  private static void buildHeap(int[] data) {
     for (int i = data.length - 1; i >= 0; i--) pushDown(data, data.length, i);
   }
 
